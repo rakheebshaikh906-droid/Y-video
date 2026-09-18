d36244b099828c679d548b392321089167d8afed
Rakheeb <rakheebshaikh906@gmail.com>
2026-09-18 13:30:15 +0530

    bugs fixes

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,10 +1,13 @@
-import { fs } from fs;
+
 import { v2 as cloudinary } from "cloudinary";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 cloudinary.config({
-    cloud_name: Process.env.CLOUDINARY_CLOUD_NAME, // Click 'View API Key' above to copy your Cloud name
-    api_key: Process.env.CLOUDINARY_API_KEY,
-    api_secret: Process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, // Click 'View API Key' above to copy your Cloud name
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
 });
 
 const uploadToCloudinary = async (filePath) => {
@@ -13,10 +16,12 @@ const uploadToCloudinary = async (filePath) => {
             resource_type: "auto", // this will automatically detect the file type (image, video, etc.)
             // folder: "youtube-clone" // optional: specify a folder in your Cloudinary account to store the uploaded files
         })
-        fs.unlinkSync(filePath); // delete the file from the local server after uploading to Cloudinary
+        console.log("File uploaded to Cloudinary successfully:", result.secure_url);
+        //fs.unlinkSync(filePath); // delete the file from the local server after uploading to Cloudinary
         return result
     } catch (error) {
-        fs.unlinkSync(filePath); // delete the file from the local server if there's an error during upload
+        console.error("Error uploading file to Cloudinary:", error);
+        //fs.unlinkSync(filePath); // delete the file from the local server if there's an error during upload
         return null;
     }
 }
