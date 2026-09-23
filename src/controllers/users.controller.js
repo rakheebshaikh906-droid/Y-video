609040ec4a8eeb263a096224476c82b7a4b82e47
Rakheeb <rakheebshaikh906@gmail.com>
2026-09-23 20:25:24 +0530

    add updateAccountDetails function to modify user profile information

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -241,7 +241,29 @@ const getCurrentuser = asyncHandler(async (req, res) => {
             "User found successfully"));
 })
 
+const updateAccountDetails = asyncHandler(async (req, res) => {
+    const { fullName, email } = req.body;
+    if (!fullName || !email) {
+        throw new ApiError(400, "fullName and email are required");
+    }
+    const user = await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            fullName, //fullName : fullName => aisa bhi lekh sakte 
+            email  //email : email => aisa bhi lekh sakte
+        },
+        {
+            new: true
+        }
+    )
+
+    return res
+        .status(200)
+        .json(new ApiResponce(200, user, "User updated successfully"));
+
+});
+
 export {
     registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword
-    , getCurrentuser
+    , getCurrentuser, updateAccountDetails
 };
